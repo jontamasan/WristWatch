9100c9cf5d236fb1ad9227311f614cad4324880e
jontamasan <levelsync@gmail.com>
2017-10-23 11:17:16 +0900

    fix and optimized with hour needle bug

--- wrist_watch/wrist_watch.cs
+++ wrist_watch/wrist_watch.cs
@@ -1,11 +1,10 @@
 ﻿using HutongGames.PlayMaker;
 using MSCLoader;
-using System.IO;
 using UnityEngine;
 
 namespace wrist_watch
 {
-    public class wrist_watch : Mod
+    public class WristWatch : Mod
     {
         public override string ID => "Wristwatch";
         public override string Name => "Wristwatch";
@@ -16,28 +15,34 @@ namespace wrist_watch
 
         Keybind _wristwatchKey = new Keybind("WristwatchKey", "Wristwatch", KeyCode.G);
 
-        private float m_hourDegrees = 30f;
-        private float m_minuteDegrees = 6f;
+        private const float HOUR_DEGREES = 30f;
+        private const float MINUTE_DEGREES = 6f;
 
+        // object
         private GameObject _WRISTWATCH;
         private GameObject _ARM;
         private GameObject _needleHours;
         private GameObject _needleMinutes;
 
+        // clock data
         private FsmFloat m_rawMinutes;
-        private FsmFloat m_fsmMinutes;
         private FsmInt m_fsmHours;
         private int m_hours;
+        private int _hourCount;
+        private float m_minutes = 0f;
+        private bool _doUpdateHour;
 
+        // flags
         private FsmBool _isPlayerSleeps;
         private FsmBool _isFsmHandLeft;
         private bool _isSlept;
         private bool _isInit = true;
 
+        // fov
         private Camera _cameraComponent;
         private float _fov;
 
-        //Called when mod is loading
+        // Called when mod is loading
         public override void OnLoad()
         {
             Keybind.Add(this, _wristwatchKey);
@@ -62,6 +67,7 @@ namespace wrist_watch
 
             if (_isInit)
             {
+                if (GameObject.Find("FPSCamera/FPSCamera") == null) return;
                 Init();
                 _isInit = false;
             }
@@ -77,13 +83,42 @@ namespace wrist_watch
                 _isSlept = false;
             }
 
-            // position.y of watch
+            // positioning pos.y of watch depending on fov
             if (_cameraComponent.fieldOfView != _fov)
             {
                 var posDelta = _cameraComponent.fieldOfView - _fov;
                 _WRISTWATCH.transform.localPosition += new Vector3(0f, -posDelta / 250f);
                 _fov = _cameraComponent.fieldOfView;
             }
+
+            // update hours
+            if (System.Math.Floor(m_rawMinutes.Value) != 0 && _doUpdateHour == false)
+            {
+                _doUpdateHour = true;
+            }
+            if (System.Math.Floor(m_rawMinutes.Value) == 0 && _doUpdateHour == true)
+            {
+                _hourCount++;
+                if (_hourCount >= 2)
+                {
+                    m_hours++;
+                    m_hours = m_hours >= 12 ? m_hours - 12 : m_hours;
+                    _hourCount = 0;
+                }
+                _doUpdateHour = false;
+            }
+            // update minutes
+            if (_hourCount == 0)
+            {
+                m_minutes = m_rawMinutes.Value / 2;
+            }
+            else
+            {
+                m_minutes = m_rawMinutes.Value / 2 + 30;
+            }
+            // rotate watch needle
+            _needleMinutes.transform.localEulerAngles = new Vector3(0f, m_minutes * MINUTE_DEGREES);
+            _needleHours.transform.localEulerAngles = new Vector3(0f, (m_hours * HOUR_DEGREES) + (m_minutes * MINUTE_DEGREES / 12));
 
             // show&hide wristwatch
             if (_wristwatchKey.IsDown())
@@ -100,12 +135,9 @@ namespace wrist_watch
                     _isFsmHandLeft.Value = true;
                 }
             }
-
-            // rotate watch needle
-            _needleMinutes.transform.localEulerAngles = new Vector3(0f, m_fsmMinutes.Value * m_minuteDegrees);
-            _needleHours.transform.localEulerAngles = new Vector3(0f, (m_hours * m_hourDegrees) + (m_fsmMinutes.Value * m_minuteDegrees / m_hourDegrees * 2));
         }
 
+        // Initialize and when player slept
         private void Init()
         {
             var FPSCamera = GameObject.Find("FPSCamera/FPSCamera");
@@ -115,7 +147,7 @@ namespace wrist_watch
             /****/
             _WRISTWATCH.transform.localPosition = new Vector3(-0.05f, -0.5f, 0.5f);
             // load clock data
-            m_rawMinutes = FsmVariables.GlobalVariables.FindFsmFloat("ClockMinutes");
+            m_rawMinutes = FsmVariables.GlobalVariables.FindFsmFloat("ClockMinutes"); // 1 = 1/2 minute, reseted per 60 = 30 minutes
             _isPlayerSleeps = FsmVariables.GlobalVariables.FindFsmBool("PlayerSleeps");
             _isFsmHandLeft = FsmVariables.GlobalVariables.FindFsmBool("PlayerHandLeft");
 
@@ -124,14 +156,23 @@ namespace wrist_watch
                 if (fsm.name == "SUN")
                 {
                     m_fsmHours = fsm.FsmVariables.GetFsmInt("Time");
-                    m_fsmMinutes = fsm.FsmVariables.GetFsmFloat("Minutes");
+                    /*m_fsmMinutes = fsm.FsmVariables.GetFsmFloat("Minutes");*/
                     break;
                 }
             }
 
-            m_hours = m_fsmHours.Value;
-            m_hours = m_hours > 12 ? m_hours - 12 : m_hours;
-            _needleHours.transform.localEulerAngles = new Vector3(0f, m_hours * m_hourDegrees);
+            _hourCount = 0;
+            m_minutes = m_rawMinutes.Value / 2;
+            m_hours = m_fsmHours.Value >= 12 ? m_fsmHours.Value - 12 : m_fsmHours.Value;
         }
+
+#if DEBUG
+        public override void OnGUI()
+        {
+            GUI.Label(new Rect(0, 20, 500, 500), "minute: " + m_minutes);
+            GUI.Label(new Rect(0, 40, 500, 500), "hour: " + m_hours);
+            GUI.Label(new Rect(0, 60, 500, 500), "hour: " + m_fsmHours.Value);
+        }
+#endif
     }
 }
